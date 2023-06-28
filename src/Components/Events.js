f3e713ceaca5d9df9f2f977c2e03b3222f652f1f
Jemienachukwu <jemieofoegbu@gmail.com>
2023-06-28 12:55:57 +0100

    update

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Card, Image, Text, Stack } from "@chakra-ui/react";
+import { Card, Image, Text, Stack, Flex } from "@chakra-ui/react";
 import { FiFileText } from "react-icons/fi";
 import campushive from "../assets/campushive.jpeg";
-import tedex from "../assets/TEDX.jpeg";
+import tedx from "../assets/TEDX.jpeg";
 const Events = () => {
-  const data = [{ img: campushive }];
+  const cardData = [
+    { img: tedx, title: "Tedx FUTO", paid: true, org: "TEDx Futo" },
+    { img: campushive, title: "Campushive", paid: false, org: "TEDx Futo" },
+    {
+      img: campushive,
+      title: "Elev8",
+      paid: true,
+      org: "Mega Student Leadership conference (MSLC)",
+    },
+  ];
   return (
-    <div>
-      <Card maxW="sm">
-        <Image
-          src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-          alt="Green double couch with wooden legs"
-          borderRadius="lg"
-          w="100%"
-        />
-        <Stack mt="6" spacing="3">
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired
-            spaces, earthy toned spaces and for people who love a chic design
-            with a sprinkle of vintage design.
-          </Text>
-        </Stack>
-      </Card>
-    </div>
+    <Flex direction={{ base: "column", md: "row" }}>
+      {cardData.map((item, i) => (
+        <Card w="300px" mx="10px">
+          <Image src={item.img} alt={item.title} borderRadius="lg" w="100%" />
+          <Stack mt="6" spacing="3">
+            <Text>
+              This sofa is perfect for modern tropical spaces, baroque inspired
+              spaces, earthy toned spaces and for people who love a chic design
+              with a sprinkle of vintage design.
+            </Text>
+          </Stack>
+        </Card>
+      ))}
+    </Flex>
   );
 };
 
